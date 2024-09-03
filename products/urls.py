33550fd56f606d19d69c4b24ff2f0012e90731bf
Barby Kelly <barbykelly1@gmail.com>
2024-09-03 09:44:32 +0000

    Added product details functionality

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
+# Code followed from Boutique Ado walk-through, like the rest of the code. Link in README
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
