33d224234c4a477709b953b2eb39a17422d8db48
Barby Kelly <barbykelly1@gmail.com>
2024-10-03 13:43:24 +0000

    Added forms, admin and signals

--- checkout/models.py
+++ checkout/models.py
@@ -9,10 +9,10 @@ from products.models import Product
 
 class Order(models.Model):
     order_number = models.CharField(max_length=15, null=False, editable=False)
-    full_name = models.CharField(max_length=50, null=False, editable=False)
-    email = models.EmailField(max_length=100, null=False, editable=False)
-    phone_number = models.CharField(max_length=20, null=False, editable=False)
-    country = models.CharField(max_length=50, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=50, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
@@ -21,6 +21,13 @@ class Order(models.Model):
             Generate a random, unique order number using UUID
             """
             return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a line item is added
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total_sum']
+        self.save()
 
     def save(self, *args, **kwargs):
         """
@@ -45,7 +52,7 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total
         """
-        self.lineitem_total = self.product.price * self.quantity()
+        self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
