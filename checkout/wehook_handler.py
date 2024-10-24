6e2d625d0278b4835df8ded5fe69cbd7ac8c0dc9
Barby Kelly <barbykelly1@gmail.com>
2024-10-24 22:32:50 +0000

    Added stripe charge object. Edited checkout models, to add delivery option and grand total like in walk-through, instead of download option and order-total only

--- checkout/wehook_handler.py
+++ checkout/wehook_handler.py
@@ -22,9 +22,77 @@ class StripeWH_Handler:
         Handle the payment_intent.succeeded webhook from Stripe
         """
         intent = event.data.object
-        print(intent)
+        pid = intent.id
+        cart = intent.metadata.cart
+        save_info = intent.metadata.save_info
+
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
+
+        billing_details = stripe_charge.billing_details # updated
+        order_total = round(stripe_charge.amount / 100, 2) # updated
+
+        order_exists = False
+        attempt = 1
+        while attempt <= 5:
+            try:
+                order = Order.objects.get(
+                    full_name_iexact=shipping_details.name,
+                    email_iexact=shipping_details.email,
+                    phone_number_iexact=shipping_details.phone,
+                    country_iexact=shipping_details.country,
+                    postcode_iexact=shipping_details.postal_code,
+                    town_or_city_iexact=shipping_details.city,
+                    street_address1_iexact=shipping_details.line1,
+                    street_address2_iexact=shipping_details.line2,
+                    county_iexact=shipping_details.state,
+                    grand_total=grand_total,
+                )
+                order_exists = True
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                    status=200)
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+        if order_exists:
+            return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                    status=200)
+        else:
+            order = None
+            try:
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    email=shipping_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.country,
+                    postcode=shipping_details.postal_code,
+                    town_or_city=shipping_details.city,
+                    street_address1=shipping_details.line1,
+                    street_address2=shipping_details.line2,
+                    county=shipping_details.state,
+                )
+                for item_id, item_data in json.loads(cart).items():
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
+
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
