4daab2ec2d033357f1c45178b8c82d1e69632766
Barby Kelly <barbykelly1@gmail.com>
2024-10-25 09:21:17 +0000

    Added details about delivery, sizes

--- cart/contexts.py
+++ cart/contexts.py
@@ -20,14 +20,35 @@ def cart_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
-    
-    order_total = total
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
 
     context = {
         'cart_items': cart_items,
         'total': total,
         'product_count': product_count,
-        'order_total': order_total,  
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': order_total,  
     }
 
     return context
