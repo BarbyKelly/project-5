3a223c45330876fa329239dfb78f34a8258697a6
BarbyKelly <barbykelly1@gmail.com>
2025-10-06 22:21:28 +0100

    Fixed S3 static storage to overwrite files, as changes were not showing in AWS S3

--- project5/custom_storages.py
+++ project5/custom_storages.py
@@ -5,10 +5,10 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     location = 'static'
     default_acl = 'public-read'
-    file-overwrite = True
+    file_overwrite = True
 
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
-    file-overwrite = False
+    file_overwrite = False
