08e3a6167ed98f729430f7e4b20e9a718d6dba96
BarbyKelly <barbykelly1@gmail.com>
2025-10-07 12:46:18 +0100

    Fixed main-nav double id, removed / from google link, fixed custom_storages.py for static to send css through to deployed ssite/S3

--- project5/custom_storages.py
+++ project5/custom_storages.py
@@ -1,14 +1,11 @@
 # Code exactly from ChatGPT, to fix css and AWS S3 connection
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = 'static'
-    default_acl = 'public-read'
-    file_overwrite = True
+    location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
-    location = 'media'
-    default_acl = 'public-read'
-    file_overwrite = False
+    location = settings.MEDIAFILES_LOCATION
